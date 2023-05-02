112f872dd41ba54dbdf25d78dc48cbb44c7aab80
axel <axelcaron7@outlook.fr>
2023-05-02 22:37:43 +0200

    Added data models

---
+++ app/src/main/java/com/example/tempoapp/model/ColorTempo.kt
@@ -0,0 +1,8 @@
+package com.example.tempoapp.model
+
+import com.google.gson.annotations.SerializedName
+
+class ColorTempo {
+    @SerializedName("couleurJourJ"  ) var couleurJourJ  : String? = null
+    @SerializedName("couleurJourJ1" ) var couleurJourJ1 : String? = null
+}
